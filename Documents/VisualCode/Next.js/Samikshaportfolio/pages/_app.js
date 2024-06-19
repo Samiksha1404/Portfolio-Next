3723bce47ff4eddf63b9879e752c49bfa9b684e6
Samiksha Gupta <guptasr_3@rknec.edu>
2024-06-19 18:04:16 +0530

    works changes

--- Documents/VisualCode/Next.js/Samikshaportfolio/pages/_app.js
+++ Documents/VisualCode/Next.js/Samikshaportfolio/pages/_app.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 import Transition from '../components/Transition'
 //router
 import { useRouter } from 'next/router';
-
+import Head from 'next/head';
 
 // framer motion 
 import { AnimatePresence, motion } from 'framer-motion';
@@ -12,6 +12,11 @@ import { AnimatePresence, motion } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
+    <>
+    <Head>
+        <title>Samiksha Gupta</title>
+        <link rel="icon" href="\sglogo.png" />
+      </Head>
     <Layout>
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
@@ -21,6 +26,7 @@ function MyApp({ Component, pageProps }) {
         </motion.div>
       </AnimatePresence>
     </Layout>
+    </>
   );
 }
 
--- Documents/VisualCode/Next.js/Samikshaportfolio/pages/index.js
+++ Documents/VisualCode/Next.js/Samikshaportfolio/pages/index.js
@@ -40,7 +40,7 @@ const Home = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            className="max-w-sm xl:max-w-xl xl:text-md sm:text-sm mx-auto xl:mx-0 mb-10 xl:mb-16">
             As a seasoned full-stack developer, I craft unique web applications that push the boundaries 
             of innovation. Dive into my portfolio to see my latest projects, read insightful articles,
              and connect with me to collaborate on turning your vision into a digital reality.
